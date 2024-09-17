ea3e28f9250974e65459ea00d88ce15b630ae91f
priooods <priodwisembodo23@gmail.com>
2024-09-18 01:16:27 +0700

    INIT

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      cgbold: ["Cgbold"],
+      cgsemibold: ["Cgsemibold"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
